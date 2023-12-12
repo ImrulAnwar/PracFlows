38c2bf2da3c0659d26642ef9fb5cb5929b0bfe36
Imrul Anwar <imrulanwar836@gmail.com>
2023-12-12 09:01:54 +0600

    Flow is emitting the Value. I can collect the emitted value from jetpack as a state.

--- app/src/main/java/com/imrul/pracflows/MainActivity.kt
+++ app/src/main/java/com/imrul/pracflows/MainActivity.kt
@@ -3,13 +3,15 @@ package com.imrul.pracflows
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.imrul.pracflows.ui.MainViewModel
 import com.imrul.pracflows.ui.theme.PracFlowsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,11 +19,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PracFlowsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                val vm = viewModel<MainViewModel>()
+                val time = vm.countDownFlow.collectAsState(initial = 10)
+
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
+                    Text(
+                            text = time.value.toString(),
+                            fontSize = 30.sp, modifier = Modifier.align(
+                        Alignment.Center))
 
                 }
             }
--- app/src/main/java/com/imrul/pracflows/ui/MainViewModel.kt
+++ app/src/main/java/com/imrul/pracflows/ui/MainViewModel.kt
@@ -1,9 +1,18 @@
 package com.imrul.pracflows.ui
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 
 class MainViewModel : ViewModel() {
-    val countDownFlow = flow<Int>{}
+    val countDownFlow = flow{
+        val startingValue = 10
+        var currentValue = startingValue
+        while (currentValue > 0) {
+            delay(1000)
+            currentValue--
+            emit(currentValue)
+        }
+    }
 
 }
